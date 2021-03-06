85baa6b1e4276499eaa92cd0ca8e5b1f6a7414dd
hiroki <raxito2008@yahoo.co.jp>
2021-03-06 16:07:14 +0900

    fix: Nuxt初期設定

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [
+		'prettier',
+		'prettier/vue',
+		'plugin:nuxt/recommended'
+	],
+	plugins: [
+	]
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['ress'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
