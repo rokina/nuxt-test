4af337aaf19d0a857dd329110d5daceab84938c6
hk-sato <hk-sato@respect-pal.jp>
2021-03-09 16:22:13 +0900

    fix: APIキーの隠蔽

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,11 @@
 <script>
 import axios from 'axios'
 export default {
-  async asyncData() {
+  async asyncData({ $config }) {
     const { data } = await axios.get(
       'https://headless-test.microcms.io/api/v1/news',
       {
-        headers: { 'X-API-KEY': '824d2944-2a90-40b2-9097-78dfb1570015' }
+        headers: { 'X-API-KEY': $config.apiKey }
       }
     )
     return data
