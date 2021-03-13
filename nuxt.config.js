e1de1531f7ea9a678c70e15e8d89a387379199fe
hiroki <raxito2008@yahoo.co.jp>
2021-03-13 11:53:18 +0900

    fix 全体に適用するCSSを分割

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['ress'],
+  css: [
+    'ress',
+    '~/assets/scss/default.scss'
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
